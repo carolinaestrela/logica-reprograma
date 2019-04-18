3398199e12c5fe4abb445bd47b3efdbe78df49b1
Carolina <estrela.carolina@bol.com.br>
2019-04-17 22:20:18 -0300

    Exercicios Logica 2

--- 02/Ex01-news/js/script.js
+++ 02/Ex01-news/js/script.js
@@ -1,9 +1,21 @@
 var mensagem = document.querySelector('.mensagem');
 
-
 var botao = document.querySelector('.button');
 botao.addEventListener('click',function(evento){
     evento.preventDefault();
     var email = document.querySelector('#newsInputEmail')
-       mensagem.textContent = 'O email '+ email.value +' foi cadastrado com sucesso!'
+// condição que verifica se o e-mail digitado está vazio ou retorna zero
+if (email.value == "" || email.value.length == 0 ){
+    mensagem.textContent = 'Digite um email válido'
+    return
+//condição que verifica se o e-mail contém o @ 
+ } else if( email.value.indexOf('@')<=-0 || email.value.indexOf('@')>=email.value.length -1){
+    mensagem.textContent= ' o e-mail informado é inválido'
+    return 
+
+ }else{
+     //concatena o e-mail com a informação de que o e-mail foi cadastrado
+    mensagem.textContent = ' o email' + email.value + ' foi cadastrado com sucesso! '
+}
+    console.log( email.value.indexOf('@'))
 })
---
+++ 02/Ex02-login/css/js/script.js
@@ -0,0 +1,27 @@
+var mensagem = document.querySelector('.mensagem');
+var botao = document.querySelector('.button');
+botao.addEventListener('click', function(evento){
+    evento.preventDefault();
+
+    var email = document.querySelector('#newsInputEmail')
+    if (email.value =="" ||email.value.length == 0) {
+
+        mensagem.textContent = 'Digite um e-mail válido'
+        return
+
+    } else if (email.value.indexOf('@')==0 || email.value.indexOf ('@')>=email.value.length){
+
+        mensagem.textContent = "O email informado é inválido"
+        return
+        
+    } else {
+        mensagem.textContent = 'O e-mail ' + email.value +'foi cadastrado.'
+    }
+
+    var senha = document.querySelector('loginInputPassword');
+    if(senha.value == "" || senha.value.length <6 ) {
+
+        mensagem.textContent = 'Digite uma senha com mais de 6 caracteres.'
+        return 
+    }
+})
